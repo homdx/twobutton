f444b69ff8db62597311cdb61acdcc684141a37d
homdx <37062532+homdx@users.noreply.github.com>
2018-10-07 23:03:15 +0300

    New main.py

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.core.text import Label as CoreLabel
+from kivy.factory import Factory
+from kivy.lang import Builder
+
+Builder.load_string("""
+<MyActivity@BoxLayout>:
+    orientation: 'vertical'
+
+    TextInput:
+        id: text_input
+
+    Button:
+        text: 'Button 1'
+        on_press: app.press_button(self, self.text)
+
+    Button:
+        text: 'Button 2'
+        on_press: app.press_button(self, self.text)
+""")
+
 
 class MyApp(App):
     def build(self):
+        self.activity = Factory.MyActivity()
+        return self.activity
 
-        fl = FloatLayout(size = (300,300))
-        fl.add_widget(Button(
-            text = "This is first button",
-            font_size = 16,
-            on_press = self.btn_press1,
-            background_color = [1, 0, 0, 1],
-            background_normal = '',
-            size_hint = (.5, .25),
-            pos = (0, 10)));
+    def press_button(self, instance, text):
+        # instance.text = 'Я нажата'
+        self.activity.ids.text_input.text = self.activity.ids.text_input.text + '%s\n' % text
 
-        fl.add_widget(Button(
-            text = "This is second button",
-            font_size = 16,
-            on_press = self.btn_press2,
-            background_color = [1, 0, 0, 1],
-            background_normal = '',
-            size_hint = (.5, .25),
-            pos = (0, 150)));
-
-
-        return fl
-
-    def btn_press1(self, instance):
-        print('Button one pressed')
-        instance.text = 'Button one pressed'
-
-    def btn_press2(self, instance):
-        print('Button second pressed')
-        instance.text = 'Button second pressed'
 
 if __name__ == "__main__":
     MyApp().run()
